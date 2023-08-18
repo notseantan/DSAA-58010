2c92c46935d9fea1b87c61c5d519833063cc27cb
Sean William P. Tan <123444479+notseantan@users.noreply.github.com>
2023-08-18 09:08:43 +0800

    Add files via upload

---
+++ Activity1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+int l, w, a;
+cout << "Please enter the values of the rectangle: ";
+cin >> l >> w; 
+a = l*w;
+cout << "Area" << " = " << a;     
+return 0;
+}
